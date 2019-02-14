a0bd6c9aabc71c71de76a67e25dcb3c0ca0b4954
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-13 23:31:35 -0500

    finished PersonHandler class

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -11,51 +11,39 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
-        String result = "";
-        // assume there is a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
-        return result;
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        
+        while (count < personArray.length) {
+            result = result.append(personArray[count].toString());
+            count++;
+        }
+        
+        return result.toString();
     }
 
 
 
     public String forLoop() {
-        String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
-        return result;
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < personArray.length; i++) {
+            result = result.append(personArray[i].toString());
+        }
+        
+        return result.toString();
     }
 
 
 
     public String forEachLoop() {
-        String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
-        return result;
+        StringBuilder result = new StringBuilder();
+        
+        for (Person p : personArray) {
+            result = result.append(p.toString());
+        }
+        
+        return result.toString();
     }
 
 
